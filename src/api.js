001599248bd041a0d8614c138cf2a4ad547fd0df
trembalaoriginal <ivsonrfilho@gmail.com>
2025-07-05 03:37:32 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // src/api.js
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://oraculobd.onrender.com';
 
 export async function executeCommand(command) {
   const response = await fetch(`${API_BASE_URL}/`, {
