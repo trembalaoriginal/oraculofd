c544d378f68ef2d3d29ad39e1d99a21fc05f6719
trembalaoriginal <ivsonrfilho@gmail.com>
2025-07-04 23:15:11 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // Importa o seu componente App.jsx
-import './App.css'; // Importa o arquivo de estilos CSS para o aplicativo
+import App from './app.jsx'; // Importa o seu componente App.jsx
+import './app.css'; // Importa o arquivo de estilos CSS para o aplicativo
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
